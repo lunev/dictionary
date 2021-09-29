95a48cc3ffcb36c34c6c6590ee7a34f3fbbc01e8
lunev <lunevdev@gmail.com>
2021-09-29 14:23:37 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { FormSearch } from './components/FormSearch';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>English Dictionary</h1>
+      <FormSearch />
+    </div>
+  );
+}
+
+
+git remote add origin https:<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>://@github.com/<lunev>/<english-dictionary>.git
---
+++ src/components/FormSearch.js
@@ -0,0 +1,74 @@
+import { useRef, useState } from "react";
+
+const fetchURL = (keyword) => {
+  return fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`)
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+export const FormSearch = (event) => {
+  const [res, setRes] = useState([]);
+  const [isLoading, SetIsLoading] = useState(false);
+  const input = useRef(null);
+  const reset = useRef(null);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    SetIsLoading(true);
+    let query = input.current.value;
+    query.length &&
+      fetchURL(query).then((result) => {
+        console.log(result);
+        setRes(result);
+        SetIsLoading(false);
+      });
+  };
+
+  const onReset = (event) => {
+    input.current.value = "";
+    console.log(res);
+    setRes([]);
+  };
+
+  return (
+    <>
+    
+      <form onSubmit={onSubmit}>
+        <input type="text" ref={input} />
+        <button type="submit">Search</button>
+        <button type="reset" onClick={onReset} ref={reset}>
+          Clear
+        </button>
+      </form>
+
+      {isLoading && "Loading..."}
+      {res &&
+        res.map((re) => {
+          const { word, origin } = re;
+          return (
+            <div className="res">
+              <h2>{word}</h2>
+              <p>{origin}</p>
+              {re.phonetics.length &&
+                re.phonetics.map((ph) => {
+                  return (
+                    <>
+                      <p>{ph.text}</p>
+                      {ph.audio && (
+                        <audio controls>
+                          <source src={ph.audio} type="audio/mpeg" />
+                        </audio>
+                      )}
+                    </>
+                  );
+                })}
+            </div>
+          );
+        })}
+    </>
+  );
+};
